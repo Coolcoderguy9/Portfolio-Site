ba60a58c4f149fcb766686fb86e80765ef9a61bb
Coolcoderguy9 <97407877+Coolcoderguy9@users.noreply.github.com>
2022-09-21 16:23:16 -0500

    Add files via upload
    Added basic contact form under contact section, simply need to integrate form validation as well as a database configuration for storing messages from interested employers or forwarding them directly via email

--- pfolio/pf.py
+++ pfolio/pf.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('main.html') 
 
-@app.route("/contact")
+@app.route("/contact", methods=('GET', 'POST'))
 def contact():
-    return render_template('contact.html')
+    fields = [{'Name': 'Please enter your name', 'Contact': 'Please supply your contact information', 'Message': 'Please provide a brief message regarding your visit'}]
+    return render_template('contact.html', fields = fields)
 
 @app.route("/about")
 def about():
