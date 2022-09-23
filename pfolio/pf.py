4f84d0cbdd56f1b65a7076e09d76e33e5fd23ca6
Coolcoderguy9 <97407877+Coolcoderguy9@users.noreply.github.com>
2022-09-23 16:13:17 -0500

    Add files via upload
    Created database for storing and updating new information and contact from employers, need to revise and fix bugs as well as improving the frontend with properly implemented scripts

--- pfolio/pf.py
+++ pfolio/pf.py
@@ -1,8 +1,14 @@
 # simple web page intended to showcase assorted personal projects
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
+import sqlite3
+
+connection = sqlite3.connect("contacts.db")
+cursor = connection.cursor()
+cursor.execute("Create Table Connections (Name TEXT, Contact TEXT, Message TEXT)")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b1aa2872746a72528e6b7db9238b91922f8fd861f26d21cd'
 
 @app.route("/")
 def home():
@@ -11,6 +17,19 @@ def home():
 @app.route("/contact", methods=('GET', 'POST'))
 def contact():
     fields = [{'Name': 'Please enter your name', 'Contact': 'Please supply your contact information', 'Message': 'Please provide a brief message regarding your visit'}]
+    if request.method == 'POST':
+        name = request.form['name']
+        contact = request.form['contact']
+        message = request.form['message']
+
+        if not name:
+            flash("Please supply a name!")
+        elif not contact:
+            flash("Please supply contact info!")
+        elif not message:
+            flash("Please leave a short message!")
+        else:
+            cursor.execute("INSERT INTO Connections VALUES ('name', 'contact', 'message')")
     return render_template('contact.html', fields = fields)
 
 @app.route("/about")
